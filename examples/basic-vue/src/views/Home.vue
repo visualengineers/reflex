720c5539b8c88c6966aecbc939f9aed2722b49b6
Mathias Müller <reworc@gmx.de>
2023-12-18 11:11:40 +0100

    Status visualized in Vue app

--- examples/basic-vue/src/views/Home.vue
+++ examples/basic-vue/src/views/Home.vue
@@ -5,17 +5,22 @@
             <TouchPoint :point="touchPoint"/>
           </div>        
         </div>
+        <div class="status__panel">
+          <Status/>
+        </div>
   </div>
 </template>
 
 <script>
 import TouchPoint from '../components/TouchPoint.vue'
+import Status from '../components/Status.vue';
 
 export default {
   name: "Home",
   inject: ["touchData"],
   components: {
-    TouchPoint
+    TouchPoint,
+    Status
   }
 };
 </script>
