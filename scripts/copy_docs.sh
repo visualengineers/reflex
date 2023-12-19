8fda4860b6b3eeaad2f2f18738e7459cab56877b
Mathias Müller <reworc@gmx.de>
2023-12-19 15:13:42 +0100

    added shell script to copy markdown docs

---
+++ scripts/copy_docs.sh
@@ -0,0 +1,9 @@
+# bash function to copy and create target directory if not exists
+CP() {
+    mkdir -p $(dirname "$2") && cp "$1" "$2"
+}
+
+CP readme.md docs/software/repo/reflex.md
+CP tools/ReFlex.TrackingServer/readme.md docs/software/apps/server.md
+CP tools/logging/README.md docs/software/apps/logging.md
+CP tools/emulator/README.md docs/software/apps/emulator.md
