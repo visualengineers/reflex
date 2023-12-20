61feff1cf99d91236cec68f4678ef5c66758bdb5
Mathias Müller <reworc@gmx.de>
2023-12-20 11:04:33 +0100

    fix: duplicate headings
    optimized styling
    fix for navigation structure

--- scripts/copy_docs.sh
+++ scripts/copy_docs.sh
@@ -3,7 +3,7 @@ CP() {
     mkdir -p $(dirname "$2") && cp "$1" "$2"
 }
 
-CP readme.md docs/software/repo/reflex.md
+CP readme.md docs/software/repo/overview.md
 CP tools/ReFlex.TrackingServer/readme.md docs/software/apps/server.md
 CP tools/logging/README.md docs/software/apps/logging.md
 CP tools/emulator/README.md docs/software/apps/emulator.md
