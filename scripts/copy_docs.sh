9401b11ef61f8bc41ed8739db87a2525665a2882
Mathias Müller <mtmu.dresden@outlook.de>
2024-01-09 22:41:28 +0100

    updated library readme + copy script

--- scripts/copy_docs.sh
+++ scripts/copy_docs.sh
@@ -4,6 +4,7 @@ CP() {
 }
 
 CP readme.md docs/software/repo/reflex.md
+CP library/README.md docs/software/repo/library.md
 CP tools/ReFlex.TrackingServer/readme.md docs/software/apps/server.md
 CP tools/logging/README.md docs/software/apps/logging.md
 CP tools/emulator/README.md docs/software/apps/emulator.md
