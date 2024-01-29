3e8f1a7dc8ea03af92bc39977b3af4ff0e0540dd
Mathias Müller <mtmu.dresden@outlook.de>
2024-01-29 18:30:26 +0100

    added list view of interactions

---
+++ examples/ExampleWPF/ViewModels/InteractionListViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using ExampleWPF.Models;
+using Prism.Ioc;
+using Prism.Mvvm;
+using ReFlex.Core.Networking.Util;
+
+namespace ExampleWPF.ViewModels;
+
+public class InteractionListViewModel : BindableBase, IDisposable
+{
+    private readonly ServerConnection _server;
+    private int _messageId = 0;
+
+    public ObservableCollection<InteractionMessage> Messages { get; } = new ObservableCollection<InteractionMessage>();
+
+    public InteractionListViewModel()
+    {
+        _server = ContainerLocator.Current.Resolve<ServerConnection>();
+        _server.ClientInstance.NewDataReceived += UpdateInteractions;
+    }
+    
+    public void Dispose()
+    {
+        _server.ClientInstance.NewDataReceived -= UpdateInteractions;
+        
+    }
+
+    private void UpdateInteractions(object? sender, NetworkingDataMessage e)
+    {
+        _messageId++;
+
+        var dispatcher = Application.Current?.Dispatcher;
+
+        dispatcher?.BeginInvoke(new Action(() =>
+        {
+            Messages.Add(new InteractionMessage(e.Message, _messageId));
+
+            var count = Messages.Count;
+
+            if (count <= 100)
+                return;
+
+            var delete = Messages.SkipLast(100);
+            foreach (var msg in delete)
+            {
+                Messages.Remove(msg);
+            }
+        }));
+    }
+}
