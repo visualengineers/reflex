bd3ccc0cb315795a6a7d9e197fc4f7e5436042d1
MajestyJonasty <lorenz.jonas74@gmx.de>
2024-04-29 23:21:55 +0200

    Problem gelöst

--- tools/gestenrekorder/lib/src/components/option-checkbox/option-checkbox.component.ts
+++ tools/gestenrekorder/lib/src/components/option-checkbox/option-checkbox.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-option-checkbox',
+  standalone: true,
   templateUrl: './option-checkbox.component.html'
 })
 export class OptionCheckboxComponent {
