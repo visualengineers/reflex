94cd5200b709fbf9eef4484b8917ed72c10db160
MajestyJonasty <lorenz.jonas74@gmx.de>
2024-04-29 20:04:39 +0200

    Problem: ngComponent aus Lib bekomm ich nicht in app importiert

--- tools/gestenrekorder/src/app/test-component/test-component.component.ts
+++ tools/gestenrekorder/src/app/test-component/test-component.component.ts
@@ -1,9 +1,22 @@
+// import { Component } from '@angular/core';
+// import { OptionCheckboxComponent } from '../../../lib/src/public-api';
+
+// @Component({
+//   selector: 'app-test-component',
+//   standalone: true,
+//   imports: [OptionCheckboxComponent],
+//   templateUrl: './test-component.component.html',
+//   styleUrl: './test-component.component.scss'
+// })
+// export class TestComponentComponent {
+
+// }
 import { Component } from '@angular/core';
+import { OptionCheckboxComponent } from '../../../lib/src/public-api';
 
 @Component({
   selector: 'app-test-component',
   standalone: true,
-  imports: [],
   templateUrl: './test-component.component.html',
   styleUrl: './test-component.component.scss'
 })
