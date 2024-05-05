0e6b857f88c99e6db4e3a69246e7bd705b07842e
Joel Giese <giese.joel@gmail.com>
2024-05-05 21:45:35 +0200

    touch-area programmiert

--- tools/gestenrekorder/src/app/app.component.ts
+++ tools/gestenrekorder/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { TouchAreaComponent } from "./touch-area/touch-area.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, TouchAreaComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'gestenrekorder';
+  title = "gestenrekorder";
 }
--- tools/gestenrekorder/src/app/touch-area/touch-area.component.ts
+++ tools/gestenrekorder/src/app/touch-area/touch-area.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { combineLatest, fromEvent, Subscription, race} from 'rxjs';
+import { distinctUntilChanged, map, filter, publishBehavior, refCount, switchMap, takeUntil, debounceTime, withLatestFrom, startWith, pairwise } from 'rxjs/operators';
+import { environment } from '../../../src/environments/environment';
+import { ExtremumType, Interaction } from '@reflex/shared-types';
+interface Size {
+  width: number;
+  height: number;
+}
 
 @Component({
   selector: 'app-touch-area',
@@ -7,6 +15,13 @@ import { Component } from '@angular/core';
   templateUrl: './touch-area.component.html',
   styleUrl: './touch-area.component.scss'
 })
-export class TouchAreaComponent {
+export class TouchAreaComponent implements OnInit, OnDestroy {
 
+  ngOnInit():void {
+    
+  }
+  
+  ngOnDestroy() {
+    
+  }
 }
