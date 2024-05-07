d9f06edca4063895418d56c8293c222b3efa9f9d
Mathias Müller <mtmu.dresden@outlook.de>
2024-05-07 21:51:46 +0200

    unsuccessfully try to import assets again

--- examples/shared-components-demo/src/app/app.component.ts
+++ examples/shared-components-demo/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OptionCheckboxComponent, PanelHeaderComponent, SettingsGroupComponent, ValueSliderComponent, ValueTextComponent } from '@reflex/angular-components/dist/@reflex/angular-components'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SettingsGroupComponent,
+    ValueSliderComponent,
+    ValueSliderComponent,
+    OptionCheckboxComponent,
+    PanelHeaderComponent,
+    ValueTextComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'test';
+  title = 'ReFlex | Shared Components Demo';
 }
