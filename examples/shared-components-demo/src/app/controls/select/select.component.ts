e004dfdc514227252c6187cac48831e48e5339cf
Mathias Müller <mtmu.dresden@outlook.de>
2024-05-09 01:13:29 +0200

    added dropdown

--- examples/shared-components-demo/src/app/controls/select/select.component.ts
+++ examples/shared-components-demo/src/app/controls/select/select.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SettingsGroupComponent, ValueSelectionComponent } from '@reflex/angular-components/dist';
+import { BehaviorSubject, Subscription, filter, timer } from 'rxjs';
 
 @Component({
   selector: 'app-select',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule, SettingsGroupComponent, ValueSelectionComponent ],
   templateUrl: './select.component.html',
   styleUrl: './select.component.scss'
 })
-export class SelectComponent {
+export class SelectComponent implements OnInit, OnDestroy {
+  public filters = [
+    "Default",
+    "Custom Special",
+    "Moving Average",
+    "SavitzyGolay",
+    "None"
+  ];
 
+  public selectedFilterIdx = 0;
+
+  public statusMessage = '';
+
+  private remainingToast: BehaviorSubject<number> = new BehaviorSubject(0);
+  private notification$: Subscription = new Subscription();
+
+  public ngOnInit(): void {
+    this.notification$.add(
+      this.remainingToast.pipe(
+        filter((value) => value === 0)
+      ).subscribe({
+        next: () => (this.statusMessage = '')
+      })
+    );
+  }
+
+  public ngOnDestroy(): void {
+      this.notification$?.unsubscribe();
+  }
+
+  public saveFilterType() {
+    this.remainingToast.next(this.remainingToast.getValue() + 1);
+    this.statusMessage = `savedFilter: ${ this.filters[this.selectedFilterIdx] } (x${this.remainingToast.getValue()})`;
+
+    timer(2000).pipe(
+      filter(() => this.remainingToast.getValue() > 0)
+    ).subscribe({
+      complete:() => ( this.remainingToast.next(this.remainingToast.getValue() - 1))
+    })
+  }
 }
