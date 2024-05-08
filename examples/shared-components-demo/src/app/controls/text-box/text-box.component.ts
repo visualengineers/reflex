b57676e51c4dc933491c709d612f8bca2595075b
Mathias Müller <mtmu.dresden@outlook.de>
2024-05-09 01:30:33 +0200

    finetuning

--- examples/shared-components-demo/src/app/controls/text-box/text-box.component.ts
+++ examples/shared-components-demo/src/app/controls/text-box/text-box.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SettingsGroupComponent, ValueTextComponent } from '@reflex/angular-components/dist';
 
 @Component({
   selector: 'app-text-box',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule, SettingsGroupComponent, ValueTextComponent ],
   templateUrl: './text-box.component.html',
   styleUrl: './text-box.component.scss'
 })
 export class TextBoxComponent {
+  public numChanges = 0;
 
+  public address = 'http://www.test-adresse.com/api';
+
+  public updateAddress() {
+    this.numChanges++;
+  }
 }
