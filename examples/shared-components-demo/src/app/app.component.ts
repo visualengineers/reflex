c72e1938475fc2de91c98d73f94b1bec23879e21
Mathias Müller <mtmu.dresden@outlook.de>
2024-05-08 15:08:47 +0200

    feature: test components - bindings are NOT working !

--- examples/shared-components-demo/src/app/app.component.ts
+++ examples/shared-components-demo/src/app/app.component.ts
@@ -1,25 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SettingsGroupComponent, ValueSliderComponent, ValueSelectionComponent, OptionCheckboxComponent, PanelHeaderComponent, ValueTextComponent } from '@reflex/angular-components/dist';
+import { OptionCheckboxComponent, PanelHeaderComponent, ReFlexSharedComponentsModule, SettingsGroupComponent, ValueSelectionComponent, ValueSliderComponent, ValueTextComponent } from '@reflex/angular-components/dist';
 import { ColorComponent } from './color/color.component';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, interval } from 'rxjs';
 import { DataService } from '../services/data.service';
 import { HttpClientModule } from '@angular/common/http';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe
+
+ } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HttpClientModule,
+    FormsModule,
     RouterOutlet,
+    ReFlexSharedComponentsModule,
     SettingsGroupComponent,
-    ValueSliderComponent,
-    ValueSelectionComponent,
-    OptionCheckboxComponent,
     PanelHeaderComponent,
+    OptionCheckboxComponent,
+    ValueSelectionComponent,
+    ValueSliderComponent,
     ValueTextComponent,
-    ColorComponent, JsonPipe, AsyncPipe
+    ColorComponent,
+    AsyncPipe
   ],
   providers: [
     DataService
@@ -34,8 +40,13 @@ export class AppComponent implements OnInit {
   public version = '1.0.0';
 
   public angular_json: BehaviorSubject<string> = new BehaviorSubject('');
-
   public component_imports: BehaviorSubject<string> = new BehaviorSubject('');
+
+  public toggleHeaderActive = true;
+  public canToggleHeader = true;
+  public toggleToast = '';
+
+  private remainingToast = 0;
 
   public constructor(private dataService: DataService) {
 
@@ -55,6 +66,21 @@ export class AppComponent implements OnInit {
       },
       error: (error) => console.error('could not load component imports text', error)
     });
+  }
+
+  public toggleHeaderChanged() {
+    console.warn(this.toggleToast);
+    this.toggleToast = 'changed';
+    this.remainingToast++;
+
+    interval(3000).subscribe({
+      next:() => {
+        if (this.remainingToast <= 1) {
+          this.toggleToast = '';
+        }
+        this.remainingToast--;
+      }
+    })
   }
 
 
