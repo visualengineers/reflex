d7ac5299b2732ba67e61666270b78714ea8f37cd
Joel Giese <giese.joel@gmail.com>
2024-05-12 19:56:11 +0200

    generated menubar

--- tools/gestenrekorder/src/app/app.component.ts
+++ tools/gestenrekorder/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { TouchAreaComponent } from "./touch-area/touch-area.component";
 import { TimelineComponent } from "./timeline/timeline.component";
+import { MenubarComponent } from "./menubar/menubar.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, TouchAreaComponent, TimelineComponent],
+  imports: [RouterOutlet, TouchAreaComponent, TimelineComponent, MenubarComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
---
+++ tools/gestenrekorder/src/app/menubar/menubar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menubar',
+  standalone: true,
+  imports: [],
+  templateUrl: './menubar.component.html',
+  styleUrl: './menubar.component.scss'
+})
+export class MenubarComponent {
+
+}
