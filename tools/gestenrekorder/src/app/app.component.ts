4984fa78b97cbd011c90e3085ca36239c37e8361
MajestyJonasty <lorenz.jonas74@gmx.de>
2024-05-13 19:14:51 +0200

    edited css, removed not need component

--- tools/gestenrekorder/src/app/app.component.ts
+++ tools/gestenrekorder/src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { TouchAreaComponent } from "./touch-area/touch-area.component";
 import { TimelineComponent } from "./timeline/timeline.component";
-import { MenubarComponent } from "./menubar/menubar.component";
 import { TrackComponentComponent } from "./track-component/track-component.component";
 import { OverlayComponent } from "./overlay/overlay.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet,TouchAreaComponent,TimelineComponent,MenubarComponent,TrackComponentComponent,OverlayComponent],
+  imports: [RouterOutlet,TouchAreaComponent,TimelineComponent,TrackComponentComponent,OverlayComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
