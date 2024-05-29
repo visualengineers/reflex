9899dfb69353fedfea5907ee51f42ebbbef2ac8d
Tim Müller <tim.mueller@stud.htw-dresden.de>
2024-05-29 18:00:29 +0200

    Implementierung neuer Timeline

--- tools/gestenrecorder/src/app/app.component.ts
+++ tools/gestenrecorder/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from "@angular/router";
 import { TouchAreaComponent } from "./touch-area/touch-area.component";
 import { DropdownComponent } from "./dropdown/dropdown.component";
 import { PullupComponent } from "./pullup/pullup.component";
+import { NewTimelineComponent } from "./new-timeline/new-timeline.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet,TouchAreaComponent,DropdownComponent,PullupComponent],
+  imports: [RouterOutlet,TouchAreaComponent,DropdownComponent,PullupComponent, NewTimelineComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
