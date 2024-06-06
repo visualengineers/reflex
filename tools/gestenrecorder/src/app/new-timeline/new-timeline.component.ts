4218f9a6604f532c6ecbbeb6217b3cabe098b7cd
MajestyJonasty <lorenz.jonas74@gmx.de>
2024-06-06 22:17:32 +0200

    editing timeline

--- tools/gestenrecorder/src/app/new-timeline/new-timeline.component.ts
+++ tools/gestenrecorder/src/app/new-timeline/new-timeline.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import * as PlotlyJS from 'plotly.js-dist-min';
 import { PlotlyModule } from 'angular-plotly.js';
+import { CommonModule } from '@angular/common';
 
 PlotlyModule.plotlyjs = PlotlyJS;
 
@@ -27,7 +28,7 @@ interface Gesture {
 @Component({
   selector: 'app-new-timeline',
   standalone: true,
-  imports: [PlotlyModule, HttpClientModule],
+  imports: [PlotlyModule, HttpClientModule, CommonModule],
   templateUrl: './new-timeline.component.html',
   styleUrl: './new-timeline.component.scss'
 })
@@ -35,6 +36,11 @@ export class NewTimelineComponent implements OnInit {
   
   public max_value_layer = 4;
   public min_value_layer = -4;
+  public horizontalPosition = 0;
+
+  public segmentsCount = 0;
+  public segmentWidth: number = 0;
+  public segments: any[] = [];
 
   public graph = {
     data: [] as any[],
@@ -65,7 +71,7 @@ export class NewTimelineComponent implements OnInit {
         gridcolor: '#bdbdbd',
         gridwidth: 1
       },
-      showlegend: false
+      showlegend: false,
     },
     config: {
       displayModeBar: false,
@@ -76,45 +82,82 @@ export class NewTimelineComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.loadGestureData();
-  }
-
-  loadGestureData() {
-    this.http.get<Gesture>('assets/data/sampleGesture.json').subscribe(data => {
-      const frames = data.tracks[0].frames;
-      
-      const numFrames = data.numFrames;
-
-      const frameIndices = Array.from({ length: numFrames }, (_, i) => i);
-
-      const normalizeAndRound = (z: number, min: number, max: number) => {
-        const normalized = ((z + 1) / 2) * (max - min) + min;
-        return Math.round(normalized);
-      };
-      const normalizedframes = frames.map(frame => ({
-        ...frame,
-        z: normalizeAndRound(frame.z, this.min_value_layer, this.max_value_layer)
-      }));
-
-      const zValues = normalizedframes.map(frame => frame.z);
-
-      console.log('Frame Indices:', frameIndices);
-      console.log('Z Values:', zValues);
-
-      this.graph.data = [
-        {
-          x: frameIndices,
-          y: zValues,
-          type: 'scatter',
-          mode: 'markers',
-          name: 'Z-Werte',
-          marker: { color: 'blue', size: 10 }, // Farbe der Marker auf blau setzen
-          customdata: normalizedframes.map(frame => `(${frame.x}, ${frame.y}, ${frame.z})`),
-          hovertemplate: 'Koordinaten: %{customdata}<extra></extra>'
-        }
-      ];
-
-      console.log('Graph data:', this.graph.data);
+    this.loadGestureData().then(() => {
+      console.log('segmentsCount', this.segmentsCount);
+      this.segmentWidth = this.graph.layout.width / this.segmentsCount;
+      for (let i = 0; i < this.segmentsCount; i++) {
+        this.segments.push(i);
+      }
     });
   }
+
+  loadGestureData(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.http.get<Gesture>('assets/data/sampleGesture.json').subscribe(data => {
+        const frames = data.tracks[0].frames;
+        
+        const numFrames = data.numFrames;
+  
+        const frameIndices = Array.from({ length: numFrames }, (_, i) => i);
+  
+        const normalizeAndRound = (z: number, min: number, max: number) => {
+          const normalized = ((z + 1) / 2) * (max - min) + min;
+          return Math.round(normalized);
+        };
+        const normalizedframes = frames.map(frame => ({
+          ...frame,
+          z: normalizeAndRound(frame.z, this.min_value_layer, this.max_value_layer)
+        }));
+  
+        const zValues = normalizedframes.map(frame => frame.z);
+  
+        console.log('Frame Indices:', frameIndices);
+        console.log('Z Values:', zValues);
+  
+        this.graph.data = [
+          {
+            x: frameIndices,
+            y: zValues,
+            type: 'scatter',
+            mode: 'markers',
+            name: 'Z-Werte',
+            marker: { color: 'blue', size: 10 },
+            customdata: normalizedframes.map(frame => `(${frame.x}, ${frame.y}, ${frame.z})`),
+            hovertemplate: 'Koordinaten: %{customdata}<extra></extra>'
+          }
+        ];
+        this.segmentsCount = zValues.length;
+        console.log('segmentsCount', this.segmentsCount);
+        resolve();
+      }, error => {
+        reject(error);
+      });
+    });
+  }
+  
+  updateHorizontalPosition(index: number) {
+    this.horizontalPosition = (index + 0.5) * this.segmentWidth;
+    console.log('horizontalPosition', this.horizontalPosition)
+    this.updateVerticalLinePosition();
+  }
+
+  updateVerticalLinePosition() {
+    const container = document.getElementById('.graph-container');
+    if (container) {
+      // TODO: wird noch nicht gefunden
+      console.log('container found')
+      const plotlyGraph = container.querySelector('.overlay');
+      const verticalLine = container.querySelector('.vertical-line');
+  
+      if (plotlyGraph instanceof HTMLElement && verticalLine instanceof HTMLElement) {
+        const plotlyGraphRect = plotlyGraph.getBoundingClientRect();
+        const plotlyGraphLeft = plotlyGraphRect.left;
+        const plotlyGraphWidth = plotlyGraphRect.width;
+        const verticalLineLeft = plotlyGraphLeft + this.horizontalPosition / this.graph.layout.xaxis.dtick * plotlyGraphWidth;
+        verticalLine.style.left = `${verticalLineLeft}px`;
+      }
+    }
+  }
+  
+  
 }
