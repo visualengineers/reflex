d1c36fc8c3e10306a2f5a0f0ab675d8e2684df27
Joel Giese <giese.joel@gmail.com>
2024-06-11 11:03:18 +0200

    implemented part of the data service

--- tools/gestenrecorder/src/app/new-timeline/new-timeline.component.ts
+++ tools/gestenrecorder/src/app/new-timeline/new-timeline.component.ts
@@ -3,6 +3,7 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import * as PlotlyJS from 'plotly.js-dist-min';
 import { PlotlyModule } from 'angular-plotly.js';
 import { CommonModule } from '@angular/common';
+import { GestureDataService } from '../service/gesture-data.service';
 
 PlotlyModule.plotlyjs = PlotlyJS;
 
@@ -12,42 +13,25 @@ interface Frame {
   z: number;
 }
 
-interface Track {
-  touchId: number;
-  frames: Frame[];
-}
-
-interface Gesture {
-  id: number;
-  name: string;
-  numFrames: number;
-  speed: number;
-  tracks: Track[];
-}
-
 @Component({
   selector: 'app-new-timeline',
   standalone: true,
   imports: [PlotlyModule, HttpClientModule, CommonModule],
   templateUrl: './new-timeline.component.html',
-  styleUrl: './new-timeline.component.scss'
+  styleUrls: ['./new-timeline.component.scss']
 })
 export class NewTimelineComponent implements OnInit {
-  
   public max_value_layer = 4;
   public min_value_layer = -4;
   public horizontalPosition = 0;
-
   public segmentsCount = 0;
   public segmentWidth: number = 0;
   public segments: any[] = [];
-
   public graph = {
     data: [] as any[],
     layout: {
       width: 800,
       height: 200,
-      //title: 'Zeitleiste',
       margin: {
         t: 0,
         b: 0,
@@ -64,14 +48,14 @@ export class NewTimelineComponent implements OnInit {
       },
       xaxis: {
         title: 'Frame',
-        range: [(0-0.5), (24-0.5)],
+        range: [-0.5, 23.5],
         tick0: 0,
         dtick: 1,
         showgrid: false,
         gridcolor: '#bdbdbd',
         gridwidth: 1
       },
-      showlegend: false,
+      showlegend: false
     },
     config: {
       displayModeBar: false,
@@ -79,7 +63,10 @@ export class NewTimelineComponent implements OnInit {
     }
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private gestureDataService: GestureDataService,
+    http: HttpClient
+  ) {}
 
   ngOnInit() {
     this.loadGestureData().then(() => {
@@ -93,51 +80,43 @@ export class NewTimelineComponent implements OnInit {
 
   loadGestureData(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-      this.http.get<Gesture>('assets/data/sampleGesture.json').subscribe(data => {
-        const frames = data.tracks[0].frames;
-        
-        const numFrames = data.numFrames;
-  
-        const frameIndices = Array.from({ length: numFrames }, (_, i) => i);
-  
-        const normalizeAndRound = (z: number, min: number, max: number) => {
-          const normalized = ((z + 1) / 2) * (max - min) + min;
-          return Math.round(normalized);
-        };
-        const normalizedframes = frames.map(frame => ({
-          ...frame,
-          z: normalizeAndRound(frame.z, this.min_value_layer, this.max_value_layer)
-        }));
-  
-        const zValues = normalizedframes.map(frame => frame.z);
-  
-        console.log('Frame Indices:', frameIndices);
-        console.log('Z Values:', zValues);
-  
-        this.graph.data = [
-          {
-            x: frameIndices,
-            y: zValues,
-            type: 'scatter',
-            mode: 'markers',
-            name: 'Z-Werte',
-            marker: { color: 'blue', size: 10 },
-            customdata: normalizedframes.map(frame => `(${frame.x}, ${frame.y}, ${frame.z})`),
-            hovertemplate: 'Koordinaten: %{customdata}<extra></extra>'
-          }
-        ];
-        this.segmentsCount = zValues.length;
-        console.log('segmentsCount', this.segmentsCount);
-        resolve();
+      this.gestureDataService.gestures$.subscribe(data => {
+        if (data.length > 0) {
+          const gesture = data[0];
+          const { frameIndices, zValues, normalizedFrames } = this.gestureDataService.processGestureData(gesture, this.min_value_layer, this.max_value_layer);
+
+          console.log('Frame Indices:', frameIndices);
+          console.log('Z Values:', zValues);
+
+          this.graph.data = [
+            {
+              x: frameIndices,
+              y: zValues,
+              type: 'scatter',
+              mode: 'markers',
+              name: 'z-Werte',
+              marker: { color: 'blue', size: 10 },
+              customdata: normalizedFrames.map((frame: Frame) => `(${frame.x}, ${frame.y}, ${frame.z})`),
+              hovertemplate: 'Koordinaten: %{customdata}<extra></extra>'
+            }
+          ];
+          this.segmentsCount = zValues.length;
+          console.log('segmentsCount', this.segmentsCount);
+          resolve();
+        } else {
+          reject('No gesture data available');
+        }
       }, error => {
         reject(error);
       });
+
+      this.gestureDataService.loadGestures();
     });
   }
-  
+
   updateHorizontalPosition(index: number) {
     this.horizontalPosition = (index + 0.5) * this.segmentWidth;
-    console.log('horizontalPosition', this.horizontalPosition)
+    console.log('horizontalPosition', this.horizontalPosition);
     this.updateVerticalLinePosition();
   }
 
@@ -146,29 +125,32 @@ export class NewTimelineComponent implements OnInit {
     if (container) {
       const plotlyGraph = container.querySelector('.overlay');
       const verticalLine = container.querySelector('.vertical-line');
-  
+
       if (plotlyGraph instanceof HTMLElement && verticalLine instanceof HTMLElement) {
-        console.log("verticalLine.style.left", verticalLine.style.left)
+        console.log("verticalLine.style.left", verticalLine.style.left);
         const plotlyGraphRect = plotlyGraph.getBoundingClientRect();
         const plotlyGraphLeft = plotlyGraphRect.left;
         const plotlyGraphWidth = plotlyGraphRect.width;
-        const verticalLineLeft = plotlyGraphLeft + this.horizontalPosition-3; // TODO: Berrechnung der Position der roten Strichs optimieren
+        const verticalLineLeft = plotlyGraphLeft + this.horizontalPosition - 3; // TODO: Berechnung der Position des roten Strichs optimieren
         verticalLine.style.left = `${verticalLineLeft}px`;
-        console.log(plotlyGraphLeft,"+",this.horizontalPosition, "/", this.graph.layout.xaxis.dtick, "*", plotlyGraphWidth, "=", verticalLineLeft)
+        console.log(plotlyGraphLeft, "+", this.horizontalPosition, "/", this.graph.layout.xaxis.dtick, "*", plotlyGraphWidth, "=", verticalLineLeft);
       }
     }
   }
 
   isPlaying: boolean = false;
-  playGesture(){
+
+  playGesture() {
     this.isPlaying = true;
-    console.log('PLAY')
+    console.log('PLAY');
   }
-  stopPlayingGesture(){
+
+  stopPlayingGesture() {
     this.isPlaying = false;
-    console.log('STOP')
+    console.log('STOP');
   }
-  resetPlayback(){
-    console.log('RESET')
+
+  resetPlayback() {
+    console.log('RESET');
   }
 }
--- tools/gestenrecorder/src/app/service/gesture-data.service.ts
+++ tools/gestenrecorder/src/app/service/gesture-data.service.ts
@@ -1,84 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { MinValidator } from '@angular/forms';
+import { Gesture } from '../data/gesture';
+import { GestureTrack } from '../data/gesture-track';
+import { GestureTrackFrame } from '../data/gesture-track-frame';
 
-interface Frame {
+interface GesturePoint {
   x: number;
   y: number;
   z: number;
 }
-
-interface Track {
-  touchId: number;
-  frames: Frame[];
-}
-
-interface Gesture {
-  id: number;
-  name: string;
-  numFrames: number;
-  speed: number;
-  tracks: Track[];
-}
-
 @Injectable({
   providedIn: 'root'
 })
 export class GestureDataService {
-  private gesturesURL = 'assets/data/sampleGesture.json';
-  private gesturesSubject = new BehaviorSubject<Gesture[]>([]);
-  gestures$ = this.gesturesSubject.asObservable();
+  private currentGestureFrames: GestureTrackFrame[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor() {}
 
-  loadGestures(): void {
-    this.http.get<Gesture[]>(this.gesturesURL).subscribe(data => {
-      this.gesturesSubject.next(data);
-    })
+  addPoint(x: number, y: number, z: number): void {
+    const newFrame: GestureTrackFrame = { x, y, z };
+    this.currentGestureFrames.push(newFrame);
   }
 
-  getGesture(id: number): Observable<Gesture | undefined> {
-    return this.gestures$.pipe(
-      map((gestures: Gesture[]) => gestures.find(gesture => gesture.id === id))
-    );
+  getCurrentGestureFrames(): GestureTrackFrame[] {
+    return this.currentGestureFrames;
   }
 
-  addGesture(newGesture: Gesture): void {
-    const gestures = this.gesturesSubject.getValue();
-    this.gesturesSubject.next([...gestures, newGesture]);
-  }
-
-  updateGesture(updatedGesture: Gesture): void {
-    const gestures = this.gesturesSubject.getValue();
-    const index = gestures.findIndex(gesture => gesture.id === updatedGesture.id);
-    if ( index !== -1 ) {
-      gestures[index] = updatedGesture;
-      this.gesturesSubject.next([...gestures]);
-    }
-  }
-
-  deleteGesture(id: number): void {
-    const gestures = this.gesturesSubject.getValue();
-    const updatedGestures = gestures.filter(gesture => gesture.id !== id);
-    this.gesturesSubject.next(updatedGestures);
-  }
-
-  normalizeAndRound(z: number, min: number, max: number): number {
-    const normalized = ((z + 1) / 2) * (max - min) + min;
-    return Math.round(normalized);
-  }
-
-  processGestureData(gesture: Gesture, minValueLayer: number, maxValueLayer: number): any {
-    const frames = gesture.tracks[0].frames;
-    const numFrames = gesture.numFrames;
-    const frameIndices = Array.from({ length: numFrames }, (_, i) => i);
-    const normalizedFrames = frames.map(frame => ({
-      ...frame,
-      z: this.normalizeAndRound(frame.z, minValueLayer, maxValueLayer)
-    }));
-    const zValues = normalizedFrames.map(frame => frame.z);
-    return { frameIndices, zValues, normalizedFrames };
+  clearCurrectGestureFrames(): void {
+    this.currentGestureFrames = [];
   }
 }
