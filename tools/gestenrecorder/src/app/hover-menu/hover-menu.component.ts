3986de5a3b982286f47df6a16d897db200693002
Joel Giese <giese.joel@gmail.com>
2024-06-22 13:26:22 +0200

    hover menu changes

--- tools/gestenrecorder/src/app/hover-menu/hover-menu.component.ts
+++ tools/gestenrecorder/src/app/hover-menu/hover-menu.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NormalizedPoint } from '../model/NormalizedPoint.model';
 import { CommonModule } from '@angular/common';
+import { SettingsGroupComponent, ValueSelectionComponent, ValueTextComponent } from '@reflex/angular-components/dist';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hover-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SettingsGroupComponent, ValueSelectionComponent, ValueTextComponent, FormsModule],
   templateUrl: './hover-menu.component.html',
   styleUrls: ['./hover-menu.component.scss']
 })
@@ -13,32 +15,32 @@ export class HoverMenuComponent {
   @Input() hoveredPoint: NormalizedPoint | null = null;
 
   getX(): string {
-    return this.hoveredPoint && typeof this.hoveredPoint.x === 'number' 
-      ? this.hoveredPoint.x.toFixed(2) 
+    return this.hoveredPoint && typeof this.hoveredPoint.x === 'number'
+      ? this.hoveredPoint.x.toFixed(2)
       : 'N/A';
   }
 
   getY(): string {
-    return this.hoveredPoint && typeof this.hoveredPoint.y === 'number' 
-      ? this.hoveredPoint.y.toFixed(2) 
+    return this.hoveredPoint && typeof this.hoveredPoint.y === 'number'
+      ? this.hoveredPoint.y.toFixed(2)
       : 'N/A';
   }
 
   getZ(): string {
-    return this.hoveredPoint && typeof this.hoveredPoint.z === 'number' 
-      ? this.hoveredPoint.z.toFixed(2) 
+    return this.hoveredPoint && typeof this.hoveredPoint.z === 'number'
+      ? this.hoveredPoint.z.toFixed(2)
       : 'N/A';
   }
 
   getTime(): string {
-    return this.hoveredPoint && this.hoveredPoint.time 
-      ? new Date(this.hoveredPoint.time).toISOString() 
+    return this.hoveredPoint && this.hoveredPoint.time
+      ? new Date(this.hoveredPoint.time).toISOString()
       : 'N/A';
   }
 
   getIndex(): string {
-    return this.hoveredPoint && typeof this.hoveredPoint.index === 'number' 
-      ? this.hoveredPoint.index.toString() 
+    return this.hoveredPoint && typeof this.hoveredPoint.index === 'number'
+      ? this.hoveredPoint.index.toString()
       : 'N/A';
   }
 }
