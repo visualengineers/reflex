f14c5edfd0ebed6bca4d40c44ec65ff0d57111d4
Joel Giese <giese.joel@gmail.com>
2024-06-23 16:04:04 +0200

    Saving & Loading Gestures

--- tools/gestenrecorder/src/app/dropdown/dropdown.component.ts
+++ tools/gestenrecorder/src/app/dropdown/dropdown.component.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SettingsGroupComponent } from '@reflex/angular-components/dist';
 import { GestureoptionsComponent } from '../gestureoptions/gestureoptions.component';
 import { RecorderoptionsComponent } from '../recorderoptions/recorderoptions.component';
+import { SavingoptionsComponent } from '../savingoptions/savingoptions.component';
 @Component({
   selector: 'app-dropdown',
   standalone: true,
-  imports: [CommonModule,SettingsGroupComponent, RecorderoptionsComponent, GestureoptionsComponent],
+  imports: [CommonModule,SettingsGroupComponent, RecorderoptionsComponent, GestureoptionsComponent, SavingoptionsComponent],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.scss'
 })
 export class DropdownComponent {
   isGestureDropdownOpen: boolean = false;
   isRecorderDropdownOpen: boolean = false;
+  isSavingDropdownOpen: boolean = false;
 
   constructor(
     private elementRef: ElementRef,
@@ -21,6 +23,7 @@ export class DropdownComponent {
   closeDropdown() {
     this.isGestureDropdownOpen = false;
     this.isRecorderDropdownOpen = false;
+    this.isSavingDropdownOpen = false;
     this.removeClickOutsideListener();
   }
 
