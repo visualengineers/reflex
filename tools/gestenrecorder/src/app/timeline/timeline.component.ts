95a1ab8e6a9d07fa987d4119f60215b99ac41522
Mathias Müller <mtmu.dresden@outlook.de>
2024-12-19 20:13:19 +0100

    fixed gesture recorder issues

--- tools/gestenrecorder/src/app/timeline/timeline.component.ts
+++ tools/gestenrecorder/src/app/timeline/timeline.component.ts
@@ -15,7 +15,7 @@ PlotlyModule.plotlyjs = PlotlyJS;
         // TODO: `HttpClientModule` should not be imported into a component directly.
         // Please refactor the code to add `provideHttpClient()` call to the provider list in the
         // application bootstrap logic and remove the `HttpClientModule` import from this component.
-        HttpClientModule, CommonModule],
+        CommonModule],
     templateUrl: './timeline.component.html',
     styleUrls: ['./timeline.component.scss']
 })
