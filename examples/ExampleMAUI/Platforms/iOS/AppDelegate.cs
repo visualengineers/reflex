7ab6a88804de6bec50106c87fb851109ec491479
Mathias Müller <mtmu.dresden@outlook.de>
2025-01-01 16:57:47 +0100

    removed mobile platforms

--- examples/ExampleMAUI/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ExampleMAUI;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-  protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
