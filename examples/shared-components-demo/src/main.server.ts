e57d3fc726abd752c9fb7b9af4b6944a9029ba8d
Mathias Müller <reworc@users.noreply.github.com>
2026-05-19 14:36:11 +0200

    update shared-components-demo to v21

--- examples/shared-components-demo/src/main.server.ts
+++ examples/shared-components-demo/src/main.server.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
-import { ApplicationRef } from '@angular/core';
+import { ApplicationRef, provideZoneChangeDetection } from '@angular/core';
 
-const bootstrap = (context: BootstrapContext): Promise<ApplicationRef> => bootstrapApplication(AppComponent, config, context);
+const bootstrap = (context: BootstrapContext): Promise<ApplicationRef> => bootstrapApplication(AppComponent, {...config, providers: [provideZoneChangeDetection(), ...config.providers]}, context);
 
 export default bootstrap;
