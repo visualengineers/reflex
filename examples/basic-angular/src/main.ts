70d903e508696940e487446814ac4b2ad306aebe
Mathias Müller <reworc@users.noreply.github.com>
2026-05-19 13:32:26 +0200

    update angular basic example

--- examples/basic-angular/src/main.ts
+++ examples/basic-angular/src/main.ts
@@ -1,18 +1,19 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 const providers = [
-  { provide: 'WEBSOCKET_URL', useValue: environment.websocketUrl , deps: [] }
+  { provide: 'WEBSOCKET_URL', useValue: environment.websocketUrl, deps: [] },
 ];
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic(providers).bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
-  
+platformBrowserDynamic(providers)
+  .bootstrapModule(AppModule, {
+    applicationProviders: [provideZoneChangeDetection()],
+  })
+  .catch((err) => console.error(err));
