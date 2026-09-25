50bae55939d1c9c4d327e13cd7ebf6a8e89c9255
Mathias Müller <reworc@users.noreply.github.com>
2026-09-25 09:48:40 +0200

    Update tools/ReFlex.TrackingServer/ClientApp/app.js

--- tools/ReFlex.TrackingServer/ClientApp/app.js
+++ tools/ReFlex.TrackingServer/ClientApp/app.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow, session, powerSaveBlocker } = require('electron');
 const url = require('url');
 const path = require('path');
-const { context } = require('three/src/nodes/core/ContextNode.js');
 
 const powerSaveId = powerSaveBlocker.start('prevent-display-sleep');
 
